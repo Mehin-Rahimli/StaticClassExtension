bc8fa74d6b761950c16531ba8094b7e60852ba38
Mehin-Rahimli <mehincr-bp217@code.edu.az>
2024-10-22 22:52:52 -0700

    Initial commit

---
+++ StaticClassTask/Helper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClassTask
+{
+    internal static class Helper
+    {
+        public static bool IsOdd(this int num)
+        {
+            if (num % 2 != 0)
+            {
+                return true;
+            }
+            return false;
+
+        }
+        public static bool IsEven(this int num)
+        {
+            if (num % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool HasDigit(this string item)
+        {
+
+            for (int i = 0; i < item.Length; i++)
+            {
+                if (Char.IsDigit(item[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+
+        }
+        public static bool CheckPassword(this string password)
+        {
+            if (password.Length >= 8)
+            {
+                for (int i = 0; i < password.Length; i++)
+                {
+
+                    if (Char.IsUpper(password[i]))/*&& Char.IsLower(password[i]) && Char.IsDigit(password[i]) && password.Length >= 8*/
+                    {
+                        for (int j = 0; j < password.Length; j++)
+                        {
+                            if (Char.IsLower(password[j]))
+                            {
+                                for (int k = 0; k < password.Length; k++)
+                                {
+                                    if (Char.IsDigit(password[k]))
+                                    {
+                                        return true;
+
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+                }
+               
+            } return false;
+        }
+
+            public static string Capitalize(this string value)
+            {
+                return value.Substring(0, 1).ToUpper() + value.Substring(1).ToLower();
+            }
+        
+    }
+}
---
+++ StaticClassTask/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace StaticClassTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(6.IsEven());
+            Console.WriteLine(3.IsOdd());
+            Console.WriteLine("Salam123".HasDigit());
+            Console.WriteLine("Salam123".CheckPassword());
+            Console.WriteLine("meHiN".Capitalize());
+        }
+    }
+}
